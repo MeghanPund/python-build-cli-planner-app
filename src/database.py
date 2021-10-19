ccd8936c153f5e744e32816a0559f5d539fb8ab2
Meghan Pund <meghan.pund@gmail.com>
2021-10-19 16:18:19 -0400

    humm

--- src/database.py
+++ src/database.py
@@ -1,3 +1,4 @@
+from reminder import PoliteReminder
 import csv
 
 def list_reminders():
@@ -13,7 +14,8 @@ def list_reminders():
         print()
 
 def add_reminder(text):
+    reminder = PoliteReminder(text)
 
     with open('reminders.csv', 'a+', newline='\n') as file:
         writer = csv.writer(file)
-        writer.writerow([text])
+        writer.writerow([reminder.text])
